65d4fc46b0cb355b12403cfa86df288c07a754b9
Kanyinsola Ibikunle <99276699+KanyinKodes@users.noreply.github.com>
2022-07-15 17:32:24 +0000

    Challenge 1 Challenge 2

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,16 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
+// Initial sum is 0
+ function sumOfNumbers(arrayOfNumbers) {
+  let sum =0;
+  for (let i=0; i< arrayOfNumbers.length; i++){
+    sum+= arrayOfNumbers[i]
+  }
+   return(sum);
+ }
+ 
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,10 +33,21 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+ function countEvenNumbers(arrayOfNumbers) {
+   let length= arrayOfNumbers.length;
+   let i=0;
+   let evenCount=0;
+ while (i<length) {
+   if (arrayOfNumbers[i]%2==0) {
+     evenCount+=1;
+    }
+     i++;
+ }
+   return(evenCount);
+ 
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+   
+  assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
